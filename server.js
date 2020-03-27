f04987ca851d13d12ebad1a01f727ea9e7237c13
Skye Aoki <skyeaoki@gmail.com>
2020-03-27 09:49:24 -0500

    Add default values for mongo URI and session secret in development

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cookieSession = require("cookie-session");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 
-const authRouter = require("./routes/auth");
-const purchasesRouter = require("./routes/purchases");
-const budgetRouter = require("./routes/budget");
+const authRouter = require('./routes/auth');
+const purchasesRouter = require('./routes/purchases');
+const budgetRouter = require('./routes/budget');
 
 const app = express();
 
@@ -19,58 +19,63 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 mongoose
-    .connect(process.env.MONGOLAB_BLACK_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
+  .connect(
+    process.env.MONGOLAB_BLACK_URI || 'mongodb://localhost:27017/budgetdriven', // fallback for development
+    { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true }
+  )
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch(err => console.log(err));
 
 // Cookie Session set up
 let sess = {
-    resave: false,
-    saveUninitialized: true,
-    cookie: {}
+  resave: false,
+  saveUninitialized: true,
+  cookie: {}
 };
 
-app.use(cookieSession({
-    name: "session",
-    secret: process.env.SESSION_SECRET, 
-    maxAge:  30 * 60 * 1000, // 30 minutes in milliseconds
+app.use(
+  cookieSession({
+    name: 'session',
+    secret: process.env.SESSION_SECRET || 'secret', // fallback for development
+    maxAge: 30 * 60 * 1000, // 30 minutes in milliseconds
     secure: false
-}));
+  })
+);
 
 // Serve static files from React
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Routers
-app.use("/api/auth", authRouter);
-app.use("/api/purchases", purchasesRouter);
-app.use("/api/budget", budgetRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/purchases', purchasesRouter);
+app.use('/api/budget', budgetRouter);
 
 // Handle any requests that don't match the ones above
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 // Error handler
 app.use(function(err, req, res, next) {
-    let errors = [];
+  let errors = [];
 
-    // Mongoose validation errors
-    if(err.name === "ValidationError") {
-        let validationErrors = Object.keys(err.errors);
-        // Push each error message to array
-        validationErrors.forEach( errorName => {
-          errors.push(err.errors[errorName].message);
-        });  
-        // Send error array
-        res.status(500).send(errors);
-    }  else {
-        // Log all other errors to console
-        console.error("Error: ", err.status, err.message || err);
-        // Send error status
-        res.sendStatus( err.status || 500 );
-    }
+  // Mongoose validation errors
+  if (err.name === 'ValidationError') {
+    let validationErrors = Object.keys(err.errors);
+    // Push each error message to array
+    validationErrors.forEach(errorName => {
+      errors.push(err.errors[errorName].message);
+    });
+    // Send error array
+    res.status(500).send(errors);
+  } else {
+    // Log all other errors to console
+    console.error('Error: ', err.status, err.message || err);
+    // Send error status
+    res.sendStatus(err.status || 500);
+  }
 });
-  
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
